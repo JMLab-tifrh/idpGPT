7e2f4f731cdb4df6f86c2abe2ca10b86f703b61c
JMLab <79298538+JMLab-tifrh@users.noreply.github.com>
2024-08-10 22:34:00 +0200

    Updated train_gpt.py

--- scripts/train_gpt.py
+++ scripts/train_gpt.py
@@ -7,13 +7,14 @@ import mmap
 import random
 import pickle
 import sys
-sys.path.append("lib")
+import argparse
 from ModelClasses import *
 from Helpers import *
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # hyper-parameters
+## touch these if you know exactly what you are doing
 block_size = 128
 batch_size = 64
 n_embd = 256
@@ -25,7 +26,6 @@ eval_interval = 1000
 dropout = 0.2
 
 # functions
-
 @torch.no_grad()
 def estimate_loss(model, train, val, block_size, batch_size, eval_iters=100):
     out = {}
@@ -48,10 +48,23 @@ def estimate_loss(model, train, val, block_size, batch_size, eval_iters=100):
     model.train()
     return out
 
-# execution
-vocab_file = "data/vocab.txt"
-text_file = "data/llps_plus.seq"
 
+# reading arguments
+parser = argparse.ArgumentParser(description="Input parameters.")
+parser.add_argument("-v", "--vocab", type=str, required=True, help="vocabulary file")
+parser.add_argument("-s", "--seq", required=True, type=str, help="sequences prepared for input. use prepare_seq.py along with a fasta file to generate this file.")
+parser.add_argument("-e", "--epochs", type=str, help="number of epochs to train", default=int(1e7))
+parser.add_argument("-o", "--output", type=str, required=True, help="output path for the model")
+parser.add_argument("-l", "--losses", type=str, required=True, help="output path for the losses")
+
+args = parser.parse_args()
+vocab_file = args.vocab
+text_file = args.seq
+epochs = int(args.epochs)
+model_out = args.output
+loss_out = args.losses
+
+# execution
 with open(vocab_file, 'r', encoding='utf-8') as f:
     vocab = f.read()
     chars = sorted(set(vocab))
@@ -70,7 +83,7 @@ model = GPTLanguageModel(vocab_size, n_embd, block_size,
 opt = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 
-iterator = trange(10)#00000)
+iterator = trange(epochs)
 losses = {"train_loss":[], "val_loss":[]}
 iterator.set_postfix_str("train_loss=----, val_loss=-----")
 
@@ -94,5 +107,5 @@ for i in iterator:
         
         iterator.set_postfix_str(f"train_loss={_['train_loss']:.6f}, val_loss={_['val_loss']:.6f}")
 
-# torch.save(model, "models/all_synthetic_GPT.pt")
-# pickle.dump(losses, open("models/losses_all_synthetic_GPT.pkl", "wb"))
+torch.save(model, model_out)
+pickle.dump(losses, open(loss_out, "wb"))
