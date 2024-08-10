11826d08bf82fec7666f4fff32ad8fce3bacd4f4
JMLab <79298538+JMLab-tifrh@users.noreply.github.com>
2024-08-10 22:33:25 +0200

    Updated gen_sequences.py

--- scripts/gen_sequences.py
+++ scripts/gen_sequences.py
@@ -3,17 +3,50 @@ import torch
 from tqdm.auto import trange, tqdm
 import pickle
 import sys
-sys.path.append("lib")
+import argparse
 from ModelClasses import *
 from Helpers import *
 from Utils import *
 
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# reading arguments
+parser = argparse.ArgumentParser(description="input parameters")
+parser.add_argument("-m", "--model", type=str, required=True, help="pre-trained model")
+parser.add_argument("-N", "--num", type=str, required=True, help="number of sequences to generate")
+parser.add_argument("-min", "--min-len", type=str, required=True, help="minimum length of the sequence(s)")
+parser.add_argument("-max", "--max-len", type=str, required=True, help="maximum length of the sequence(s)")
+parser.add_argument("-voc", "--vocab", type=str, required=True, help="vocabulary use to train the model")
+parser.add_argument("-o", "--output", type=str, required=True, help="output FASTA file (.fa/.fasta)")
+parser.add_argument("-d", "--device", type=str, required=False, default="auto",
+                    help="device to run the model on (cpu/gpu/auto). [default=auto]")
 
-model = torch.load("models/no_PS_GPT.pt")
+args = parser.parse_args()
 
-with open('data/vocab.txt', 'r', encoding='utf-8') as f:
+model_file = args.model
+vocab_file = args.vocab
+N = int(args.num)
+min_len = int(args.min_len)
+max_len = int(args.max_len)
+output = args.output
+dev = args.device
+
+if dev == "auto":
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+elif dev == "gpu":
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        print("GPU selected, but not available. switching device to cpu.")
+        device = torch.device("cpu")
+elif dev == "cpu":
+        device = torch.device("cpu")
+else:
+    print("wrong input for device. exiting.")
+    exit(0)
+
+model = torch.load(model_file).to(device)
+
+with open(vocab_file, 'r', encoding='utf-8') as f:
     vocab = f.read()
     chars = sorted(set(vocab))
 vocab_size = len(chars)
@@ -23,12 +56,8 @@ int2string = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [string2int[c] for c in s]
 decode = lambda l: ''.join([int2string[i] for i in l])
 
-# generate sequences for validation
-N = 100
-min_len = 60
-max_len = 70
-
-with open("/home/wasim/idp-gpt/sims/no_PS/generated_sequences.fa", "w") as w:
+lengths = []
+with open(output, "w") as w:
     pbar = trange(N)
     for i in range(N):
         prompt = vocab[np.random.randint(len(vocab))]
@@ -38,8 +67,13 @@ with open("/home/wasim/idp-gpt/sims/no_PS/generated_sequences.fa", "w") as w:
                                                 max_new_tokens=length-1, block_size=128,
                                                 device=device)[0].tolist())
         try:
-            w.write(f">prot{i:d}\n"+replace_res(generated_chars)+"\n")
+            corrected_seq = replace_res(generated_chars)
+            w.write(f">prot{i:d}\n"+ corrected_seq +"\n")
             pbar.update()
+            lengths.append(len(corrected_seq))
         except:
             i -= 1
-        
+       
+print(f"""generated sequences saved to {output}.
+Minimum length of generated sequences = {min(lengths):d}
+Maximum length of generated sequences = {max(lengths):d}""")
